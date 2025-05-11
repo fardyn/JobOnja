d9e8d218f9313ca50ed411f27ca71427b768f219
fardyn <your-email@example.com>
2025-05-11 13:35:00 +0330

    refactor navbar with probs and atributes

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,23 +6,14 @@
             <a href="{{url('/')}}">Jobonja</a>
         </h1>
         <nav class="hidden md:flex items-center space-x-4">
-            <a href="{{url('/jobs')}}" class="text-white hover:underline py-2 {{request()->is("jobs") ? "text-yellow-500 font-bold" : ""}}">All Jobs</a>
-            <a href="{{url('/jobs/saved')}}" class="text-white hover:underline py-2 {{request()->is("jobs/saved") ? "text-yellow-500 font-bold" : ""}}"
-            >Saved Jobs</a
-            >
-            <a href="{{url('/login')}}" class="text-white hover:underline py-2 {{request()->is("login") ? "text-yellow-500 font-bold" : ""}}">Login</a>
-            <a href="{{url('/register')}}" class="text-white hover:underline py-2 {{request()->is("register") ? "text-yellow-500 font-bold" : ""}}"
-            >Register</a
-            >
-            <a href="{{url('/dashboard')}}" class="text-white hover:underline py-2 {{request()->is("dashboard") ? "text-yellow-500 font-bold" : ""}}">
-                <i class="fa fa-gauge mr-1"></i> Dashboard
-            </a>
-            <a
-                href="{{url('jobs/create')}}"
-                class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"
-            >
-                <i class="fa fa-edit"></i> Create Job
-            </a>
+            <x-nav-link url="/" :active="request()->is('/')">Home</x-nav-link>
+            <x-nav-link url="/jobs" :active="request()->is('jobs')">All jobs</x-nav-link>
+            <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')">Saved jobs</x-nav-link>
+            <x-nav-link url="/login" :active="request()->is('login')">Login</x-nav-link>
+            <x-nav-link url="/register" :active="request()->is('register')">register</x-nav-link>
+            <x-nav-link url="/dashboard" :active="request()->is('dashboard') " icon='gauge'>dashboard</x-nav-link>
+
+            <x-button-link url="/jobs/create" type="button" icon="edit">Create Job</x-button-link>
         </nav>
         <button id="hamburger" class="text-white md:hidden flex items-center">
             <i class="fa fa-bars text-2xl"></i>
