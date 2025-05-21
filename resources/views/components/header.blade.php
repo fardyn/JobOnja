e5de12d6e82cdfc536fba1ca38e7be3d1f34b844
fardyn <your-email@example.com>
2025-05-21 14:58:52 +0330

    add javascript to make menue dynamic

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -7,7 +7,7 @@
         </h1>
         <nav class="hidden md:flex items-center space-x-4">
             <x-nav-link url="/" :active="request()->is('/')">Home</x-nav-link>
-            <x-nav-link url="/jobs" :active="request()->is('jobs')">All jobs</x-nav-link>
+            <x-nav-link url="/jobs" :active="request()->is('jobs')" >All jobs</x-nav-link>
             <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')">Saved jobs</x-nav-link>
             <x-nav-link url="/login" :active="request()->is('login')">Login</x-nav-link>
             <x-nav-link url="/register" :active="request()->is('register')">register</x-nav-link>
@@ -24,22 +24,11 @@
         id="mobile-menu"
         class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2"
     >
-        <a href="{{url('/jobs')}}" class="block px-4 py-2 hover:bg-blue-700">All Jobs</a>
-        <a href="{{url('/jobs/saved')}}" class="block px-4 py-2 hover:bg-blue-700"
-        >Saved Jobs</a
-        >
-        <a href="{{url('/dashboard')}}" class="block px-4 py-2 hover:bg-blue-700"
-        >Dashboard</a
-        >
-        <a href="{{url('/login')}}" class="block px-4 py-2 hover:bg-blue-700">Login</a>
-        <a href="{{url('/register')}}" class="block px-4 py-2 hover:bg-blue-700"
-        >Register</a
-        >
-        <a
-            href="{{url('/jobs/create')}}"
-            class="block px-4 py-2 bg-yellow-500 hover:bg-yellow-600 text-black"
-        >
-            <i class="fa fa-edit"></i> Create Job
-        </a>
+        <x-nav-link url="/jobs" :active="request()->is('jobs') " :mobile="true">All jobs</x-nav-link>
+        <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')" :mobile="true">Saved jobs</x-nav-link>
+        <x-nav-link url="/login" :active="request()->is('login')" :mobile="true">Login</x-nav-link>
+        <x-nav-link url="/register" :active="request()->is('register')" :mobile="true">register</x-nav-link>
+        <x-nav-link url="/dashboard" :active="request()->is('dashboard') " icon='gauge' :mobile="true">dashboard</x-nav-link>
+        <x-button-link url="/jobs/create" type="button" icon="edit" :block="true">Create Job</x-button-link>
     </div>
 </header>
